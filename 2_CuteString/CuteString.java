e4c5ca521de1ee04b967e9c4a1b9a781a142fb00
vboled <vboled@gmail.com>
2023-02-14 23:14:55 +0300

    New algorithm (still not work)

--- 2_CuteString/CuteString.java
+++ 2_CuteString/CuteString.java
@@ -13,19 +13,35 @@ public class CuteString {
         int result = 0;
         byte[] bytes = str.getBytes();
 
-        for (int i = 0; i < str.length() && str.length() - i >= result; i++) {
+        for (int i = 0; i < str.length(); i++) {
             int interim = 1;
             int replacements = k;
+            int lastFit = -1;
+            boolean isSignFirst = true;
 
-            for (int j = i + 1; j < str.length(); j++) {
+            for (int j = str.length() - 1; j > i; j--) {
                 if (bytes[i] != bytes[j]) {
-                    if (replacements == 0)
-                        break;
+                    if (replacements == 0) {
+                        replacements = k;
+                        interim = 1;
+                        j = lastFit;
+                        lastFit = -1;
+                        isSignFirst = true;
+                    }
                     replacements--;
+                } else {
+                    if (isSignFirst) {
+                        isSignFirst = false;
+                    } else {
+                        lastFit = lastFit == -1 ? j : lastFit;
+                        System.out.print(lastFit + " ");
+                        System.out.print(j + ", ");
+                    }
                 }
                 interim++;
                 result = result < interim ? interim : result;
             }
+            System.out.println("i = " + i);
         }
         System.out.println(result);
     }
