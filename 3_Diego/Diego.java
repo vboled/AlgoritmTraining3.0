63a4f549d85bc23f1f7f0f852f02ce88a22e9c0a
vboled <vboled@gmail.com>
2023-02-18 20:49:19 +0300

    The simplest algorithm (TL)

--- 3_Diego/Diego.java
+++ 3_Diego/Diego.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Diego {
 
@@ -15,32 +14,24 @@ public class Diego {
         for (int i = 0; i < collectionSize; i++) {
             treeSet.add(scanner.nextInt());
         }
-        List<Integer> num = treeSet.stream()
-                .sorted()
-                .collect(Collectors.toList());
 
         int n = scanner.nextInt();
 
-        int[][] traders = new int[n][2];
+        List<Integer> traders = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
-            traders[i][0] = i;
-            traders[i][1] = scanner.nextInt();
+            traders.add(scanner.nextInt());
         }
-        Arrays.sort(traders, Comparator.comparing(l -> l[1]));
-
-        int res = 0, j = 0;
 
         for (int i = 0; i < n; i++) {
-            while (j < num.size() && traders[i][1] > num.get(j)) {
-                res++;
-                j++;
+            int res = 0;
+            for (Integer num : treeSet) {
+                if (num < traders.get(i)) {
+                    res++;
+                } else {
+                    break;
+                }
             }
-            traders[i][1] = res;
-        }
-
-        Arrays.sort(traders, Comparator.comparing(l -> l[0]));
-        for (int i = 0; i < n; i++) {
-            System.out.println(traders[i][1]);
+            System.out.println(res);
         }
     }
 }
