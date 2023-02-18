61a183378cbbc656bf9240c1c882c44c8c643d2a
vboled <vboled@gmail.com>
2023-02-18 15:22:31 +0300

    New implementation (TL)

--- 3_Diego/Diego.java
+++ 3_Diego/Diego.java
@@ -1,47 +1,46 @@
-import com.sun.security.jgss.InquireSecContextPermission;
-
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Diego {
 
     public static final String inputFileName = "input.txt";
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(new File("input.txt"));
+        Scanner scanner = new Scanner(new File(inputFileName));
+
+        int collectionSize = scanner.nextInt();
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < collectionSize; i++) {
+            treeSet.add(scanner.nextInt());
+        }
+        List<Integer> num = treeSet.stream()
+                .sorted()
+                .collect(Collectors.toList());
 
         int n = scanner.nextInt();
 
-        List<Integer> numList = new ArrayList<>(n);
-        while (n-- > 0) {
-            numList.add(scanner.nextInt());
+        int[][] traders = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            traders[i][0] = i;
+            traders[i][1] = scanner.nextInt();
+        }
+        Arrays.sort(traders, Comparator.comparing(l -> l[1]));
+
+        int res = 0, j = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (j < num.size() && traders[i][1] > num.get(j)) {
+                res++;
+                j++;
+            }
+            traders[i][1] = res;
         }
 
-        SortedSet<Integer> setNumbers = new TreeSet<>(numList);
-
-        n = scanner.nextInt();
-        while (n-- > 0) {
-            int mark = scanner.nextInt();
-
-            if (mark > setNumbers.last()) {
-                System.out.println(setNumbers.size());
-            } else {
-                Iterator<Integer> myIt = setNumbers.iterator();
-
-                Integer res = 0;
-
-                while (myIt.hasNext()) {
-                    if (mark <= myIt.next()) {
-                        System.out.println(res);
-                        break;
-                    }
-                    res++;
-                }
-            }
+        Arrays.sort(traders, Comparator.comparing(l -> l[0]));
+        for (int i = 0; i < n; i++) {
+            System.out.println(traders[i][1]);
         }
     }
 }
